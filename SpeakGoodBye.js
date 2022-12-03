845ac6758df011f4349f02de787eee37b501a1fb
emma <jhj191058@gmail.com>
2022-12-03 18:51:18 +0900

    feat : make hello method

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -3,7 +3,15 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
+(function(window){
+    var speakWord = "Good Bye"
+    var byeSpeaker = {};
+    byeSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    }
 
+    window.byeSpeaker = byeSpeaker;
+})(window);
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
